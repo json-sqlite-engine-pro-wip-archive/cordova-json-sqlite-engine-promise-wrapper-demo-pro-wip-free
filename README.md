7b16882461687b40513fd4adace0fa4873df7124
Christopher J. Brody <chris@brody.consulting>
2019-08-26 10:50:44 -0400

    Start Cordova json sqlite engine testapp PRO WIP
    based on: cordova-sqlite-test-app

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Cordova sqlite test app
+# Cordova json sqlite memory engine testapp PRO WIP free
 
-Template to demonstrate reproducible issues with Cordova sqlite plugin (using JQuery and Bootstrap 3). May be used as a starter project.
+XXX TBD XXX XXX
 
 **AUTHOR:** [@brodybits (Christopher J. Brody aka Chris Brody)](https://github.com/brodybits)
 
-**LICENSE:** [CC0 1.0 (public domain)](https://creativecommons.org/publicdomain/zero/1.0/)
-
-**NOTE:** This project includes JQuery (3.3.1) and Bootstrap 3 (3.3.7) under the MIT license. Otherwise there is no code copied from other sources.
+**LICENSE:** GPL v3 or commercial
 
 **IMPORTANT:** Whitelist and intent items are omitted from this test app.
 
@@ -14,8 +12,8 @@ Template to demonstrate reproducible issues with Cordova sqlite plugin (using JQ
 
 - Bootstrap (3.3.7) - included (MIT license)
 - JQuery (3.3.1) - included (MIT license)
-- `cordova-plugin-dialogs` - specified in `config.xml`
-- Cordova sqlite plugin - please add the desired version from the Cordova CLI as described below
+- `cordova-plugin-dialogs` - now specified in `package.json`
+- JSON SQLite engine plugin - (...)
 
 IMPORTANT NOTE: `cordova-plugin-dialogs` was added using the `--save` flag to ensure that this plugin would be tracked in `config.xml` for automatic installation. It is recommended to use the `--save` flag on Cordova pre-7.0 to track any other plugins added in `config.xml` (this is automatic starting with Cordova `7.0`). It is NOT recommended to commit `plugins` or `platforms` directories to git or any other form of source code control.
 
@@ -29,27 +27,25 @@ cordova plugin add my-plugin-id --save
 
 ## How to run
 
-1. Add the desired Cordova sqlite plugin version, for example:
+* Add the desired JSON SQLite engine plugin version, for example:
 
 ```shell
-cordova plugin add cordova-sqlite-storage --save
+cordova plugin add ../json-sqlite-memory-engine-wrapper-pro-wip-free
 ```
 
-NOTE: `--save` flag is automatic and not needed in Cordova 7.0 and greater
-
-2. Add the desired platform(s), for example:
+* Add the desired platform(s), for example:
 
 ```shell
 cordova platform add android
 ```
 
-3. Do `cordova prepare` (may not always be necessary but good to be on the safe side):
+* Do `cordova prepare` (seems to be needed for "osx" (macOS) platform at this time):
 
 ```shell
 cordova prepare
 ```
 
-4. Run it on your mobile emulator or device, for example:
+* Run it on your mobile emulator or device, for example:
 
 ```shell
 cordova run android
@@ -59,14 +55,11 @@ cordova run android
 
 - Upon startup: open a database and CREATE the test table
 - Alert dialog test (native alert if possible)
-- Echo test
-- Self test
 - Location reload
 - String test 1
 - String test 2 (string as a SQL parameter)
-- Show record
+- Show record count
 - Add record
-- Add 100 records from JavaScript object after delay
 - Delete all records
 - Follow link to page 2
 - Change window.location to page 2
--- www/app.js
+++ www/app.js
@@ -1,28 +1,15 @@
-var database = null;
+var databaseHandle = null;
 
 var nextUser = 101;
 
 function initDatabase() {
-  database = window.sqlitePlugin.openDatabase({name: 'sample.db', location: 'default'});
-
-  database.transaction(function(transaction) {
-    transaction.executeSql('CREATE TABLE SampleTable (name, score)');
-  });
-}
-
-function echoTest() {
-  window.sqlitePlugin.echoTest(function() {
-    showMessage('Echo test OK');
-  }, function(error) {
-    showMessage('Echo test ERROR: ' + error.message);
-  });
-}
-
-function selfTest() {
-  window.sqlitePlugin.selfTest(function() {
-    showMessage('Self test OK');
-  }, function(error) {
-    showMessage('Self test ERROR: ' + error.message);
+  window.jsonSQLiteEngine.openDatabaseHandle([':memory:'], function(x) {
+    databaseHandle = x[0];
+    window.jsonSQLiteEngine.jsonExecuteStatement([
+      databaseHandle,
+      JSON.stringify(
+        [null, 'CREATE TABLE SampleTable (name, score)', 0, null])
+    ]);
   });
 }
 
@@ -31,97 +18,66 @@ function reload() {
 }
 
 function stringTest1() {
-  database.transaction(function(transaction) {
-    transaction.executeSql("SELECT upper('Test string') AS upperText", [], function(ignored, resultSet) {
-      showMessage('Got upperText result (ALL CAPS): ' + resultSet.rows.item(0).upperText);
-    });
+  window.jsonSQLiteEngine.jsonExecuteStatement([
+    databaseHandle,
+    JSON.stringify(
+      [null, "SELECT upper('Test string') AS upperText", 0, null])
+  ], function(res) {
+    var rs = JSON.parse(res);
+    showMessage('received upperText result value (ALL CAPS): ' + rs[0].rows[0].upperText);
   }, function(error) {
-    showMessage('SELECT count error: ' + error.message);
+    showMessage('SELECT value error: ' + error.message);
   });
 }
 
 function stringTest2() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('SELECT upper(?) AS upperText', ['Test string'], function(ignored, resultSet) {
-      showMessage('Got upperText result (ALL CAPS): ' + resultSet.rows.item(0).upperText);
-    });
+  window.jsonSQLiteEngine.jsonExecuteStatement([
+    databaseHandle,
+    JSON.stringify(
+      [null, 'SELECT upper(?) AS upperText', 1, 'Test string', null])
+  ], function(res) {
+    var rs = JSON.parse(res);
+    showMessage('received upperText result value (ALL CAPS): ' + rs[0].rows[0].upperText);
   }, function(error) {
-    showMessage('SELECT count error: ' + error.message);
+    showMessage('SELECT value error: ' + error.message);
   });
 }
 
 function showCount() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('SELECT count(*) AS recordCount FROM SampleTable', [], function(ignored, resultSet) {
-      showMessage('RECORD COUNT: ' + resultSet.rows.item(0).recordCount);
-    });
+  window.jsonSQLiteEngine.jsonExecuteStatement([
+    databaseHandle,
+    JSON.stringify(
+      [null, 'SELECT count(*) AS recordCount FROM SampleTable', 0, null])
+  ], function(res) {
+    var rs = JSON.parse(res);
+    showMessage('RECORD COUNT: ' + rs[0].rows[0].recordCount);
   }, function(error) {
     showMessage('SELECT count error: ' + error.message);
   });
 }
 
 function addRecord() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('INSERT INTO SampleTable VALUES (?,?)', ['User '+nextUser, nextUser]);
-  }, function(error) {
-    showMessage('INSERT error: ' + error.message);
-  }, function() {
+  window.jsonSQLiteEngine.jsonExecuteStatement([
+    databaseHandle,
+    JSON.stringify(
+      [null, 'INSERT INTO SampleTable VALUES (?,?)', 2, 'User '+nextUser, nextUser, null])
+  ], function(res) {
     showMessage('INSERT OK');
     ++nextUser;
-  });
-}
-
-function addJSONRecordsAfterDelay() {
-  function getJSONObjectArray() {
-    var COUNT = 100;
-    var myArray = [];
-
-    for (var i=0; i<COUNT; ++i) {
-      myArray.push({name: 'User '+nextUser, score: nextUser});
-      ++nextUser;
-    }
-
-    return myArray;
-  }
-
-  function getJSONAfterDelay() {
-    var MY_DELAY = 150;
-
-    var d = $.Deferred();
-
-    setTimeout(function() {
-      d.resolve(getJSONObjectArray());
-    }, MY_DELAY);
-
-    return $.when(d);
-  }
-
-  // NOTE: This is similar to the case when an application
-  // fetches the data over AJAX to populate the database.
-  // IMPORTANT: The application MUST get the data before
-  // starting the transaction.
-  getJSONAfterDelay().then(function(jsonObjectArray) {
-    database.transaction(function(transaction) {
-      $.each(jsonObjectArray, function(index, recordValue) {
-        transaction.executeSql('INSERT INTO SampleTable VALUES (?,?)',
-          [recordValue.name, recordValue.score]);
-      });
-    }, function(error) {
-      showMessage('ADD records after delay ERROR');
-    }, function() {
-      showMessage('ADD 100 records after delay OK');
-    });
+  }, function(error) {
+    showMessage('INSERT error: ' + error.message);
   });
 }
 
 function deleteRecords() {
-  database.transaction(function(transaction) {
-    transaction.executeSql('DELETE FROM SampleTable');
+  window.jsonSQLiteEngine.jsonExecuteStatement([
+    databaseHandle,
+    JSON.stringify(
+      [null, 'DELETE FROM SampleTable', 0, null])
+  ], function(res) {
+    showMessage('DELETE OK');
   }, function(error) {
     showMessage('DELETE error: ' + error.message);
-  }, function() {
-    showMessage('DELETE OK');
-    ++nextUser;
   });
 }
 
@@ -141,15 +97,15 @@ function showMessage(message) {
 
 document.addEventListener('deviceready', function() {
   $('#alert-test').click(alertTest);
-  $('#echo-test').click(echoTest);
-  $('#self-test').click(selfTest);
+
   $('#reload').click(reload);
+
   $('#string-test-1').click(stringTest1);
   $('#string-test-2').click(stringTest2);
   $('#show-count').click(showCount);
   $('#add-record').click(addRecord);
-  $('#add-json-records-after-delay').click(addJSONRecordsAfterDelay);
   $('#delete-records').click(deleteRecords);
+
   $('#location-page2').click(goToPage2);
 
   initDatabase();
